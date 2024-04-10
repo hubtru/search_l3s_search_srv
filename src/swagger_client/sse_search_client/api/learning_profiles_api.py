70d61bea86ba842afe7682639868d3be44ac2a2b
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2024-04-10 15:53:57 +0200

    fixed swagger api -> changed some functions to correct output type

--- src/swagger_client/sse_search_client/api/learning_profiles_api.py
+++ src/swagger_client/sse_search_client/api/learning_profiles_api.py
@@ -299,7 +299,7 @@ class LearningProfilesApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type=None,  # noqa: E501
+            response_type='LearningProfileDto',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
