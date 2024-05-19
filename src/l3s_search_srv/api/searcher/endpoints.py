74d7bc359efbb9a29d6a6ba44251cd4062f130f8
Peng-LUH <ethaniel.unihannover@gmail.com>
2024-05-19 18:00:35 +0200

    update-02 20240519

--- src/l3s_search_srv/api/searcher/endpoints.py
+++ src/l3s_search_srv/api/searcher/endpoints.py
@@ -31,7 +31,7 @@ class CheckSecretKey(Resource):
     @ns_searcher.expect(parser_secret)
     def get(self):
         request_data = parser_secret.parse_args()
-        if request_data["secret_key"] == os.getenv('L3S_SEARCH_SRV_KEY'):
+        if request_data["secret_key"] == os.getenv('L3S_API_KEY'):
             return {"message": "valid secret key"}, HTTPStatus.OK
         else:
             return {"message": "invalid secret key"}, HTTPStatus.BAD_REQUEST
@@ -55,7 +55,7 @@ class SearcherUpdate(Resource):
             pprint(request_data)
             ns_searcher.logger.info("Starting: Checking client secret...")
             print(request_data["secret"])
-            if request_data["secret"] != os.getenv('MLS_CLIENT_SECRET'):
+            if request_data["secret"] != os.getenv('L3S_API_KEY'):
                 raise ValueError("Invalid secret key!")
 
             ns_searcher.logger.info("Success: client secret valid.")
