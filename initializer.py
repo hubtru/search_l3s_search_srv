74ac10ad98c269f8e4d0c29431ef52b258092acf
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2024-05-21 00:21:37 +0200

    updated initilizer to download all models and tokenizer

--- initializer.py
+++ initializer.py
@@ -1,8 +1,15 @@
 import os, requests
-from transformers import AutoModel, AutoTokenizer, AutoModelWithLMHead
+from transformers import AutoModel, AutoTokenizer
 
 ## download the pre-trained models
-models = ["dbmdz/bert-base-german-cased", "xlm-roberta-base", "dbmdz/german-gpt2-faust"]
+models = ["dbmdz/bert-base-german-cased",
+          "xlm-roberta-base",
+          "dbmdz/german-gpt2-faust",
+          "ikim-uk-essen/geberta-xlarge",
+          "dbmdz/bert-base-german-uncased",
+          "google-bert/bert-base-multilingual-uncased",
+          "google-bert/bert-base-multilingual-cased"]
 
 for model in models:
     tokenizer = AutoTokenizer.from_pretrained(model)
+    model = AutoModel.from_pretrained(model)
